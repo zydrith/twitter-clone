d68be91ba13542f9ff9095cf8c7e91240717cc23
Zhiyad Harith <zeeharith@gmail.com>
2023-04-18 14:05:54 +0800

    add modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,3 +1,16 @@
+import { BooleanLiteral } from "typescript";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+}
+
 const Modal = () => {
   return (
     <div>Modal</div>
